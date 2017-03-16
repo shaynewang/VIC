0a66c71960aec963dac4b842417bbf97a366f5ce
bobbyesh <eshleman@pdx.edu>
2017-03-15 21:22:14 -0700

    Added kernel function

--- vic/vic_run/src/example.cu
+++ vic/vic_run/src/example.cu
@@ -4,10 +4,18 @@ extern "C" {
 #include "vic_run.h"
 }
 
+
+__global__
+void some_kernel(void) {
+    int x = 1;
+    x += 1;
+}
+
 using namespace std;
 
 extern "C" 
 void example_print() {
     cout << "Cuda is linked <---------" << endl;
+    some_kernel<<<1, 1>>>();
 }
 
